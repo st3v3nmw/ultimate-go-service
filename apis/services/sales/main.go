2d2fea285679a5d23a805ab8d725ccecf667f67a
Stephen Mwangi <mail@stephenmwangi.com>
2024-08-07 16:13:21 +0300

    commit 7

--- apis/services/sales/main.go
+++ apis/services/sales/main.go
@@ -36,7 +36,7 @@ func main() {
 }
 
 func run(ctx context.Context, log *logger.Logger) error {
-	log.Info(ctx, "startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
+	log.Info(ctx, "startup", "GOMAXPROCS", runtime.GOMAXPROCS(0), "build", build)
 
 	shutdown := make(chan os.Signal, 1)
 	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
